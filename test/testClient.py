f2d7d7b62875ad45c306b58dc4121716fbdf86ad
Austin Suarez <amsuarez@outlook.com>
2018-05-06 23:20:47 -0700

    simple client done

--- test/testClient.py
+++ test/testClient.py
@@ -7,9 +7,13 @@ clientSocket = socket(AF_INET,SOCK_STREAM)
 
 clientSocket.connect((serverName,serverPort))
 
-data = "Hello World"
+data = "Hello world! This is a very long string."
+#Python 3.6 requires byte-object so message needs to be encoded
+data = data.encode('ASCII')
 bytesSent =0
 while bytesSent != len(data):
+    print(data)
     bytesSent += clientSocket.send(data[bytesSent:])
+    print(bytesSent)
 
 clientSocket.close()
