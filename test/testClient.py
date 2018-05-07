4f7ad1492cfb936fe77801a3ca82f16f1a5c5a79
Austin Suarez <amsuarez@outlook.com>
2018-05-06 22:00:57 -0700

    test client side

--- test/testClient.py
+++ test/testClient.py
@@ -1,15 +1,15 @@
-import socket
+from socket import *
 
-serverName = ecs.fullerton.edu
+serverName = "192.168.0.2"
 serverPort = 12000
 
 clientSocket = socket(AF_INET,SOCK_STREAM)
 
-clientSocket.connect((serverName,socketPort))
+clientSocket.connect((serverName,serverPort))
 
 data = "Hello World"
-
+bytesSent =0
 while bytesSent != len(data):
     bytesSent += clientSocket.send(data[bytesSent:])
 
-clientSocket..close()
+clientSocket.close()
